437f2e816da911ebe34c764093f10621aadd6d78
shub78910 <64699670+shub78910@users.noreply.github.com>
2021-02-01 17:18:49 +0530

    final build

--- app.js
+++ app.js
@@ -1,89 +1,85 @@
-console.log("Welcome to notes app. This is app.js");
-showNotes();
+let addbtn = document.querySelector(".addbtn")
+shownotes()
 
-// If user adds a note, add it to the localStorage
-let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  notesObj.push(addTxt.value);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-//   console.log(notesObj);
-  showNotes();
-});
-
-// Function to show elements from localStorage
-function showNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let html = "";
-  notesObj.forEach(function(element, index) {
-    html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                    </div>
-                </div>`;
-  });
-  let notesElm = document.getElementById("notes");
-  if (notesObj.length != 0) {
-    notesElm.innerHTML = html;
-  } else {
-    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
-  }
-}
-
-// Function to delete a note
-function deleteNote(index) {
-//   console.log("I am deleting", index);
-
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-
-  notesObj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
-}
+//event listener for add btn, which will take ip and store it in localStorage.
+addbtn.addEventListener("click", function (e) {
+    let notes = localStorage.getItem("notes");
+    let inptxtarea = document.querySelector(".input");
 
 
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none";
-        }
-        // console.log(cardTxt);
-    })
+    if (notes === null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(inptxtarea.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    inptxtarea.value = "";
+    // console.log(notesObj);   
+    shownotes()
 })
 
-/*
-Further Features:
-1. Add Title
-2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server 
-*/ 
+//this func, basically displays all the notes stored in local storage.
+
+function shownotes() {
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    // notessub.innerHTML = localStorage.getItem("notes")
+
+    // notessub.addEventListener("blur", function () {
+    //     localStorage.setItem("notes", this.innerHTML);
+    // })
+
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="output_boxes">
+        <div class="flex_child">
+        <h5>Note ${index + 1}</h5>
+        <p class="pbox">${element}</p>
+        <button id="${index}" onClick="dltnote(this.id)" class="dltbtn">Delete</button>
+        </div>
+        </div>`
+    });
+    // let notessub = document.getElementById("notes")
+    let notessub = document.querySelector(".notessub")
+
+    if (notesObj.length != 0) {
+        notessub.innerHTML = html;
+
+        // console.log(html)
+    }
+    else {
+        notessub.innerHTML = `Please add notes using "Add notes" button!`
+    }
+}
+
+//func for deleting notes.
+
+function dltnote(index) {
+    f = confirm("Do you really want to delete this note?")
+    if (f === true) {
+
+        // console.log("delte",index)
+
+        let notes = localStorage.getItem("notes");
+
+        if (notes == null) {
+            notesObj = [];
+        }
+        else {
+            notesObj = JSON.parse(notes);
+        }
+        notesObj.splice(index, 1)
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        shownotes()
+    }
+    }
