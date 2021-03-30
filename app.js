87e78d199724fd216f910f58daf3525017f4bd5d
Shubham <shubhamhirakki@gmail.com>
2021-03-30 14:42:51 +0530

    added search notes feature

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 let addbtn = document.querySelector(".addbtn")
 let inptxtarea = document.querySelector(".input");
-
+let title = document.getElementById("title");
 shownotes()
 
 //event listener for add btn, which will take ip and store it in localStorage.
 addbtn.addEventListener("click", function (e) {
     let notes = localStorage.getItem("notes");
+
     let inptxtarea = document.querySelector(".input");
-    if (inptxtarea.value.trim()!=0){
+    if (inptxtarea.value!=0){
         if (notes === null) {
             notesObj = [];
         }
         else {
             notesObj = JSON.parse(notes);
         }
-        notesObj.push(inptxtarea.value);
+        let dict ={
+            title:title.value,
+            text:inptxtarea.value
+        } 
+        notesObj.push(dict);
         localStorage.setItem("notes", JSON.stringify(notesObj));
+    
         inptxtarea.value = "";
+        title.value="";
         // console.log(notesObj); 
     }  
+    else{
+        alert("Add your text first")
+    }
     shownotes()
 })
 
 //this func, basically displays all the notes stored in local storage.
 
-function shownotes() {
+function shownotes(lis) {
     let notes = localStorage.getItem("notes");
+
 
     if (notes == null) {
         notesObj = [];
@@ -33,20 +44,17 @@ function shownotes() {
     else {
         notesObj = JSON.parse(notes);
     }
+//     console.log(notes)
+// console.log(notesObj)
 
-    // notessub.innerHTML = localStorage.getItem("notes")
-
-    // notessub.addEventListener("blur", function () {
-    //     localStorage.setItem("notes", this.innerHTML);
-    // })
 
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
         <div class="output_boxes">
         <div class="flex_child">
-        <h5 style="font-size:1.5rem">Note ${index + 1}</h5>
-        <p class="pbox">${element}</p>
+        <h5 class="titlebox" style="font-size:1.5rem">${element.title}</h5>
+        <p class="pbox">${element.text}</p>
         <button class="dltbtn tooltip" id="edit" onClick="edit(${index})">Edit Note
         <span class="tooltiptext">Scroll above to edit and save.</span>
         </button>
@@ -55,7 +63,7 @@ function shownotes() {
         </div>
         </div>`
     });
-    // let notessub = document.getElementById("notes")
+
     let notessub = document.querySelector(".notessub")
 
     if (notesObj.length != 0) {
@@ -77,7 +85,8 @@ function edit(index){
 
     let notes = localStorage.getItem("notes");
     notesObj = JSON.parse(notes);
-    inptxtarea.value = notesObj[index]
+    // console.log(notesObj[index].text)
+    inptxtarea.value =notesObj[index].text;
     addbtn.style.display = "none"
     savebtn.style.display = "block"
     saveind.value = index;
@@ -138,3 +147,58 @@ function dltnote(index) {
         shownotes()
     }
     }
+
+
+
+    //Search notes feature:
+
+    let searchRes = document.querySelector(".searchRes")
+    let searchInp = document.querySelector(".searchInp")
+    let searchop = document.querySelector(".searchop")
+    let searchtitle = document.querySelector(".searchtitle")
+    let searchtext = document.querySelector(".searchtext")
+
+
+    function searchInpfunc(e){
+        notesObj.map((elem)=>{
+            if (elem.title===e || elem.text===e){
+                console.log("sddddd")
+                searchop.style.display="block"
+                searchtitle.innerHTML = elem.title;
+                searchtext.innerHTML = elem.text;
+            }
+            else if(e===""){
+                searchop.style.display="none"
+
+            }
+        })
+        // console.log(notesObj[0].title)
+    }
+// localStorage.setItem("dix1",JSON.stringify({"name":"shubham","roll":"1"}))
+// // console.log(JSON.parse(dix1))
+// console.log(JSON.parse(localStorage.getItem("dix1")))
+
+
+// function setItemBetter(key,value)
+// {
+//     localStorage.setItem(key,JSON.stringify(value))
+// }
+
+// function getItemBetter(key)
+// {
+//     return JSON.parse(localStorage.getItem(key))
+// }
+
+
+// obj = {
+//     "name":"shub",
+//     "numb":"23322",
+//     "place":"mumbai"
+// }
+
+
+
+// setItemBetter("dix1",obj)
+// console.log(getItemBetter("dix1"))
+
+
